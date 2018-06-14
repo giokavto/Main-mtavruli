e08272de55f40361fe321a7f9e2bb14e3d5bd97d
giokavto <giorgi.kavtaradze08b@geolab.edu.ge>
2018-06-14 23:02:13 +0400

    ch

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,27 @@ $(document).ready(function() {
         navigationPosition: 'right',
     });
 });
+
+
+// burger
+$(document).ready(function() {
+
+    // Menu script
+    $("#burger").click(function() {
+        $(".anim").toggleClass("active");
+    });
+    $(".menu__item").click(function() {
+        $(".anim").toggleClass("active");
+    });
+
+    //Scroll to ID
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        var id = $(this).attr('href');
+        var $id = $(id);
+        if ($id.length === 0){return;}
+        e.preventDefault();
+        //var pos = $id.offset().top - 60;
+        var pos = $id.offset().top;
+        $('body, html').animate({scrollTop: pos});
+    });
+});
